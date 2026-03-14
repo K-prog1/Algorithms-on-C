00190c47e9a97e446aa0faf6ae6af5ecf1df0bc2
Kupaki <gercovdaniil21@gmail.com>
2026-03-15 00:12:15 +1000

    Вторая часть

--- Labs2/C1.c
+++ Labs2/C1.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 int main() {
     double matrix3x3[3][3];
+    int matrix2x2[2][2];
+    double result[2][2] = {0};
     int i,j,k;
     double summMain = 0, summDiagonal = 0;
+
     printf("\n Введите 9 чисел для матрицы 3 на 3 \n");
     for (i = 0; i < 3; i++)
     {
@@ -19,6 +22,34 @@ int main() {
     }
     printf("сумма основной диагонали: %lf", summMain);
     printf("сумма вторичной диагонали: %lf", summDiagonal);
+
+    printf("\n Введите 4 числа для матрицы 2 на 2 \n");
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j<2; j++)
+        {
+            scanf("%i", &matrix2x2[i][j]);
+        }
+    }
+
+    for (i = 0; i<2; i++){
+        for (j=0;j<2;j++){
+            for (k = 0;k<2;k++){
+                result[i][j] = matrix2x2[i][k]*matrix2x2[k][i];
+            }
+
+        }
+    }
+
+    printf("\nКвадрат матрицы:\n");
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 2; j++) {
+            printf("%.2f ", result[i][j]);
+        }
+        printf("\n");
+    }
+
+
 
     return 0;
 }   
