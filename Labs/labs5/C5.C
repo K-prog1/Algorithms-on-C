634c487bfcc718ab8a5c3133ba65a227490af12e
Kupaki <gercovdaniil21@gmail.com>
2026-03-27 22:06:17 +1000

    Фикс багов

--- Labs/labs5/C5.C
+++ Labs/labs5/C5.C
@@ -2,38 +2,40 @@
 #include <string.h>
 #include <malloc.h>
 #include <stdlib.h>
+#include "C5.h"
 
 int main(){
-    double* A, *B;
+    double *A, *B, *C;
     int i,j,n;
-    char* sgn;
+    char sgn;
 
     system("chcp 1251");
     system("cls");
 
     printf("Введите размер матрицы:\n");
-    scanf("%lf", n);
+    scanf("%d", &n);
 
 
     A = (double*)malloc(n*n*sizeof(double));
+
     B = (double*)malloc(n*n*sizeof(double));
 
     for (i = 0; i < n; i++ ){
         for (j = 0; j < n; j++){
-            printf("A[%lf][%lf] = ", i,j);
+            printf("Введите значения матрицы A[%d][%d] = ", i,j);
             scanf("%lf", (A + i*n + j));
         }
     }
 
     for (i = 0; i < n; i++ ){
         for (j = 0; j < n; j++){
-            printf("B[%lf][%lf] = ", i,j);
+            printf("Введите значения матрицы B[%d][%d] = ", i,j);
             scanf("%lf", (B + i*n + j));
         }
     }
     printf("Введите + - * (сумма вычитание или умножение матриц %c \n)", sgn);
-    scanf("%c", &sgn);
-    double* C = calculate(A, B, sgn, n);
+    scanf(" %c", &sgn);
+    C = calculate(A, B, sgn, n);
     
     for (i=0;i<n;i++){
         for (j=0;j<n;j++){
@@ -42,4 +44,5 @@ int main(){
         printf("\n");
     }
     free(A); free(B); free(C);
+    return 0;
 }
--- Labs/labs5/C5.h
+++ Labs/labs5/C5.h
@@ -1,6 +1,6 @@
-#ifndef calculator
-#define calculator
+#ifndef C5_H
+#define C5_H
 
-double* calculate(double* A, double* B, char sgn, int n, int m);
+double* calculate(double* A, double* B, char op, int n);
 
 #endif
