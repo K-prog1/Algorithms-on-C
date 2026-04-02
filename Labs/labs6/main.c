effd464ead80defb823150403bf07548b43909c0
Kupaki <gercovdaniil21@gmail.com>
2026-04-02 22:39:53 +1000

    Mission completed + respect

--- Labs/labs6/main.c
+++ Labs/labs6/main.c
@@ -1,23 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-
-    FILE *in = fopen("input.txt", "r" );
+int main() {
+    FILE *in = fopen("input.txt", "r");
     FILE *out = fopen("output.txt", "w");
+    if (!in || !out) { perror("Сучка, файлы не открыты"); return 1; }
 
     char surname[51], name[51], patronym[51];
-
     int year;
 
-    while(scanf("%s %s %s %d", surname, name, patronym, &year)==4)
-    {
-        if(year > 1980){
-            fprintf(out,"%s %s %s %d", surname, name, patronym, year);
+    while (fscanf(in, "%50s %50s %50s %d", surname, name, patronym, &year) == 4) {
+        if (year > 1980) {
+            fprintf(out, "%s %s %s %d\n", surname, name, patronym, year);
         }
-
-
     }
+
     fclose(in);
     fclose(out);
     return 0;
